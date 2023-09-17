009229d8c51e180356de8da7fe1b4d4334a74f08
Philip Solomatnikov <philip.solomatnikov@gmail.com>
2023-09-16 19:40:41 -0700

    Initial commit

---
+++ src/main/java/net/pasws/examplemod/ExampleModClient.java
@@ -0,0 +1,10 @@
+package net.pasws.examplemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExampleModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
